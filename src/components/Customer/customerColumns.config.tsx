3399e52e10daa518816d96548282501277298e5a
Badescu Cosmin <badescupetrecosmin@gmail.com>
2023-12-20 00:13:12 -0800

    cleanup

--- src/components/Customer/customerColumns.config.tsx
+++ src/components/Customer/customerColumns.config.tsx
@@ -1,7 +1,6 @@
 import { ColDef, ValueGetterParams } from "@ag-grid-community/core";
 import { useMemo } from "react";
 import { CustomerProps } from "./customer.types";
-import { EmailCellRender } from "./emailCellRenderer.component";
 
 export const useColumns = (): ColDef<CustomerProps>[] =>
   useMemo(
@@ -21,7 +20,26 @@ export const useColumns = (): ColDef<CustomerProps>[] =>
       {
         field: "email",
         enableCellChangeFlash: true,
-        cellRenderer: EmailCellRender,
+        cellRenderer: (props: { value: string }) => (
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="14"
+              height="14"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              style={{ marginRight: "5px" }}
+            >
+              <rect x="1" y="3" width="22" height="18" rx="2" ry="2" />
+              <polyline points="1 10 12 15 23 10" />
+            </svg>
+            {props.value}
+          </div>
+        ),
       },
       {
         field: "phone",
